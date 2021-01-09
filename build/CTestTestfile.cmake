e761703e3b93b99a546a0e2c66de7dfbee8cd48f
Manel Puig <manel.puig@ub.edu>
2021-01-09 20:29:51 +0100

    sat5

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/rubot_opencv_ws/src
+# Build directory: /home/mpuig/rubot_opencv_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_essentials_cpp")
+subdirs("usbcam")
---
+++ build/usbcam/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/rubot_opencv_ws/src/usbcam
+# Build directory: /home/mpuig/rubot_opencv_ws/build/usbcam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
