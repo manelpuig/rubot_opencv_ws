010f1a2256b616694810373f4223bdddd75f7798
Manel Puig <manel.puig@ub.edu>
2021-03-23 17:00:32 +0100

    deleted build & devel folders

--- build/ros_essentials_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mpuig/rubot_opencv_ws/src/ros_essentials_cpp
-# Build directory: /home/mpuig/rubot_opencv_ws/build/ros_essentials_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
